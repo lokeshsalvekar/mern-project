fd9e0311b93d2cc129246642cc5fcc1e461d5bc2
lokeshsalvekar <127538714+lokeshsalvekar@users.noreply.github.com>
2023-04-20 17:16:23 +0530

    updated

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from 'path';
+import {fileURLToPath} from 'url';
 // const express = require('express')
 // const colors = require('colors')
 
@@ -16,6 +17,11 @@ dotenv.config();
 
 // //databse config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 //rest object
 const app = express();
